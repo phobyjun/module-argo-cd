7310083fa1202f53d1aea20ee7a39dcddcee49b6
phobyjun <phobyjun@gmail.com>
2022-02-01 02:06:05 +0900

    ArgoCD module init

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "kubernetes" {
+  config_path            = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+  }
+}
+provider "helm" {
+  kubernetes {
+    config_path            = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "junseok"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argo"
+}
